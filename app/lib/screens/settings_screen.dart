8b7d8d9f547fa680ba48c1b00e245a721c9404c2
popchami <47472197+popchami@users.noreply.github.com>
2026-06-22 09:44:41 +0900

    Modernize settings screen constructor

--- app/lib/screens/settings_screen.dart
+++ app/lib/screens/settings_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../providers/theme_provider.dart';
 
 class SettingsScreen extends ConsumerWidget {
-  const SettingsScreen({Key? key}) : super(key: key);
+  const SettingsScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
