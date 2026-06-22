1df5d7227c6b027c9327d5c3a45905b78fb2c75e
popchami <47472197+popchami@users.noreply.github.com>
2026-06-22 10:55:03 +0900

    Add wear history data layer

---
+++ app/lib/providers/wear_log_provider.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../models/wear_log.dart';
+import '../repositories/wear_log_repository.dart';
+
+final wearLogRepositoryProvider = Provider<WearLogRepository>((ref) {
+  return WearLogRepository();
+});
+
+final wearLogsByShoeIdProvider =
+    FutureProvider.family<List<WearLog>, int>((ref, shoeId) async {
+  final repository = ref.watch(wearLogRepositoryProvider);
+  return repository.getWearLogsByShoeId(shoeId);
+});
+
+final recentWearLogsProvider = FutureProvider<List<WearLog>>((ref) async {
+  final repository = ref.watch(wearLogRepositoryProvider);
+  return repository.getRecentWearLogs();
+});
